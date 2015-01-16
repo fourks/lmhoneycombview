27602ba43967c300b395cbd07078817b181252ca
John Pope <jp@bellgeorge.com>
2015-01-16 16:08:53 +1100

    compile for yosemite

--- view/LMHexCell.h
+++ view/LMHexCell.h
@@ -11,27 +11,27 @@
 @class LMHoneycombView;
 
 @interface LMHexCell : NSObject {
-  NSPoint             mCentre;
-  CGFloat             mRadius;
-  NSBezierPath        *mPath;
-  int                 mCol;
-  int                 mRow;
-  id                  mData;
-  BOOL                mSelected;
-  BOOL                mDirty;
+    NSPoint mCentre;
+    CGFloat mRadius;
+    NSBezierPath *mPath;
+    int mCol;
+    int mRow;
+    id mData;
+    BOOL mSelected;
+    BOOL mDirty;
 }
 
 - (id)initWithColumn:(int)col row:(int)row;
 - (id)initWithColumn:(int)col row:(int)row data:(id)data;
 
-@property (readonly,getter=centre) NSPoint mCentre;
-@property (readonly,getter=radius) CGFloat mRadius;
-@property (readonly,getter=path) NSBezierPath *mPath;
-@property (readonly,getter=col) int mCol;
-@property (readonly,getter=row) int mRow;
-@property (getter=data,setter=setData:) id mData;
-@property (getter=selected,setter=setSelected:) BOOL mSelected;
-@property (getter=dirty,setter=setDirty:) BOOL mDirty;
+@property (readonly, getter = centre) NSPoint mCentre;
+@property (readonly, getter = radius) CGFloat mRadius;
+@property (readonly, getter = path) NSBezierPath *mPath;
+@property (readonly, getter = col) int mCol;
+@property (readonly, getter = row) int mRow;
+@property (nonatomic, assign, getter = data, setter = setData:) id mData;
+@property (getter = selected, setter = setSelected:) BOOL mSelected;
+@property (getter = dirty, setter = setDirty:) BOOL mDirty;
 
 - (void)setHexCentre:(NSPoint)centre radius:(CGFloat)radius;
 - (void)drawOnHoneycombView:(LMHoneycombView *)view withAttributes:(NSMutableDictionary *)attributes;
